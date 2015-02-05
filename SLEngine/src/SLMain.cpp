2373047100801d5a1b55b68b12e295f0c89c323d
Benjamin <brpylko@gmail.com>
2015-02-05 18:41:33 -0500

    Working depth buffer SLEngine just contains an example of this

--- SLEngine/src/SLMain.cpp
+++ SLEngine/src/SLMain.cpp
@@ -1,8 +1,18 @@
 
 #include "SLMain.hpp"
 #include <SLMINX/Graphics/Color.hpp>
+#include <SLMINX/Graphics/TextureBatch.hpp>
+#include <SLMINX/Graphics/Texture2D.hpp>
+#include <SLMINX/Graphics/ShaderFactory.hpp>
 
 using namespace StarLegacy;
+using namespace MINX;
+using namespace MINX::Graphics;
+
+
+TextureBatch* texb;
+Texture2D* tex;
+Texture2D* tex2;
 
 
 SLMain::SLMain() : Game::Game()
@@ -14,13 +24,14 @@ SLMain::SLMain() : Game::Game()
 
 void SLMain::Initialize()
 {
-	//Put stuff here that should happen when the Game is initialized.
 	Game::Initialize();
 }
 
 void SLMain::LoadContent()
 {
-	//Put stuff here that loads content for your game.
+	texb = new TextureBatch(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
+	tex = new Texture2D("test.png");
+	tex2 = new Texture2D("test2.png");
 	Game::LoadContent();
 }
 
@@ -39,7 +50,8 @@ void SLMain::Update(GameTime * gameTime)
 void SLMain::Draw(GameTime * gameTime)
 {
 	SetRenderTarget(NULL, Graphics::Color::CornflowerBlue);
-
-	//Put stuff here to draw your game each frame.
+	texb->Draw(tex, Vector2(20, 20), Vector2(sin(gameTime->GetElapsedMillis()/1000.0)+1,cos(gameTime->GetElapsedMillis()/1000.0)+1)*540);
+	texb->Draw(tex2, Vector2(80, 20), Vector2(540,540));
+	texb->DrawLoadedTextures();
 	Game::Draw(gameTime);
 }
